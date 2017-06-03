ccccfeb9a03906bd649d51bab5865b92104fdab6
probuse <nprobuse@gmail.com>
2017-06-03 13:11:31 +0300

    my view now returns the index.html file

--- employee_records/views.py
+++ employee_records/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello there')
-	# return render(request, 'employee_records/index.html')
+	# return HttpResponse('Hello there')
+	return render(request, 'employee_records/index.html')
