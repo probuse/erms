80cfffb3b6b3b3fe777f0c3d1959922b754f8082
probuse <nprobuse@gmail.com>
2017-06-03 15:06:56 +0300

    added Task model class

--- employee_records/models.py
+++ employee_records/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+TASK_STATUS = (
+	('n', 'New'),
+	('o', 'Ongoing'),
+	('c', 'Completed')
+)
+
+class Task(models.Model):
+	title = models.CharField(max_length=100)
+	assigned_task = models.TextField()
+	pub_date = models.DateField()
+	status = models.CharField(max_length=1, choices=TASK_STATUS)
+
+	def __str__(self):
+		return self.title
