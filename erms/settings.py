cb25b3cffa2bdf04cf2557a1660e8e189a059e25
probuse <nprobuse@gmail.com>
2017-06-03 13:13:49 +0300

    added employee_record.apps.EmployeeRecordConfig in INSTALLED_APPS

--- erms/settings.py
+++ erms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'employee_records.apps.EmployeeRecordsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'erms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
