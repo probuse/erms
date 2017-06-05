9612d81908df0571a829cce7e72c0219309abcf4
probuse <nprobuse@gmail.com>
2017-06-05 07:38:55 +0300

    Added Employee model that uses the User model, added functionality to assign groups to employee or group

--- employee_records/models.py
+++ employee_records/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
+
 
 TASK_STATUS = (
 	('n', 'New'),
@@ -6,10 +8,30 @@ TASK_STATUS = (
 	('c', 'Completed')
 )
 
+class Employee(models.Model):
+	user = models.OneToOneField(
+		User,
+		verbose_name='Employee Name', 
+		on_delete=models.CASCADE
+	)
+	department = models.CharField(max_length=100)
+
+	class Meta:
+		pass
+
 class Task(models.Model):
 	title = models.CharField(max_length=100)
 	assigned_task = models.TextField()
 	pub_date = models.DateField()
+	assign_to_employee = models.ForeignKey(
+		Employee, 
+		#default='employee',
+		on_delete=models.CASCADE
+	)
+	assign_to_group = models.ForeignKey(
+		Group,
+		on_delete=models.CASCADE
+	)
 	status = models.CharField(max_length=1, choices=TASK_STATUS)
 
 	def __str__(self):
