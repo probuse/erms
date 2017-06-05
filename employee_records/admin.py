feac26e1d7f6ab024e11f70804fbcdb00c87b47e
probuse <nprobuse@gmail.com>
2017-06-05 07:36:48 +0300

    renamed site header and site title, registered Task and Employee models

--- employee_records/admin.py
+++ employee_records/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Employee
 
+
+admin.site.site_header = 'erms admin'
+admin.site.site_title = 'erms site admin'
+# class TaskInline(admin.TabularInline):
+# 	model = Task
+# 	fk_name = 'assign_to_employee'
+
+
+# class TaskAdmin(admin.ModelAdmin):
+# 	inlines = [
+# 		TaskInline,
+# 	]
 
 admin.site.register(Task)
+admin.site.register(Employee)
