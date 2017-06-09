486c7a13113c3d262f7a2537c0d86500bc954649
probuse <nprobuse@gmail.com>
2017-06-09 12:18:29 +0300

    included the accounts url in the urlpatterns

--- erms/urls.py
+++ erms/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+	url(r'^accounts/', include('registration.backends.default.urls')),
 	url(r'^employee_records/', include('employee_records.urls')),
     url(r'^admin/', admin.site.urls),
     
