fe7220a37dac3732d88253d3111b23a29f15caf0
probuse <nprobuse@gmail.com>
2017-06-09 12:14:11 +0300

    added login url, added tasks url

--- employee_records/urls.py
+++ employee_records/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^tasks/(?P<pk>[0-9]+)/$', views.user_tasks, name='tasks'),
+	url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
 ]
