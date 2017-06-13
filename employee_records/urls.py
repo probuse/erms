bc31f2b32b27114411f7ab4315787921f5136b2c
probuse <nprobuse@gmail.com>
2017-06-13 22:36:29 +0300

    removed all redundant urls

--- employee_records/urls.py
+++ employee_records/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
 	url(r'^logout/$', auth_views.logout, {'next_page': 'employee_records:login'}, name='logout'),
 	url(r'^new_tasks/$', views.new_tasks, name='new_tasks'),
 	url(r'^new_tasks/(?P<task_id>[0-9]+)/$', views.task_detail, name='task_detail'),
-	# url(r'^tasks/new_tasks/(?P<task_id>[0-9]+)$', views.new_tasks, name='new_tasks'),
-	url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+	
 ]
