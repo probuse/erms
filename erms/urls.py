4f106cb00d46ecb0569151afde6e18539181a5ac
probuse <nprobuse@gmail.com>
2017-06-13 22:41:29 +0300

    cleaned employee record url

--- erms/urls.py
+++ erms/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^accounts/', include('registration.backends.default.urls')),
-	url(r'^employee_records/', include('employee_records.urls')),
+	url(r'employee_records/', include('employee_records.urls')),
     url(r'^admin/', admin.site.urls),
     
 ]
